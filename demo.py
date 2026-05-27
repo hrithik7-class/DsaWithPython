7bab3e14408a823bbc2848e3ef827dc1584c9afa
hrithik7-class <kesarwaniraj11@gmail.com>
2026-05-27 15:22:18 +0530

    add class oops

--- demo.py
+++ demo.py
@@ -1,41 +1,154 @@
-# x =5
+# # x =5
 
-# def print_x():
-#     global x
-#     x =7
-#     print(x)
+# # def print_x():
+# #     global x
+# #     x =7
+# #     print(x)
     
-# print_x()
-# print(x)    
+# # print_x()
+# # print(x)    
 
 
-# f =range(10)
-# g={"ram","syam","skrishnaMurti"}
-# h=frozenset({"ram","syam","skrishnaMurti"})
+# # f =range(10)
+# # g={"ram","syam","skrishnaMurti"}
+# # h=frozenset({"ram","syam","skrishnaMurti"})
 
-# print(f, type(f))
-# print(g, type(g))
-# print(h, type(h))
+# # print(f, type(f))
+# # print(g, type(g))
+# # print(h, type(h))
 
-# x  = "hello world"
-# print(x.lower() )
+# # x  = "hello world"
+# # print(x.lower() )
 
-# a = "Hello, World!"
-# b = a.split(",")
-# print(type(b))
+# # a = "Hello, World!"
+# # b = a.split(",")
+# # print(type(b))
 
 
 
-# x = ["banana", "apple", "grape", "orange"]
-# [print(i) for i in x if "o" in i]
+# # x = ["banana", "apple", "grape", "orange"]
+# # [print(i) for i in x if "o" in i]
+
+# # thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# # thislist.sort(reverse = True)
+# # print(thislist)
+
+# l1 = [1,2,3,4,5]
+# l2 = [6,7,8,9,10]
+
+# for x in l1:
+#     l2.append(x)
+# print(l2)
+
+
+
+
+# # python data types ..
+# int , float , complex
+# None
+# str 
+# list , tuple , range
+# bool
+
+# dict - like object
+# set
+
+
+a="raj is good"
+
+print(a[4:])
+bro =45
+
+# b ="hello bro wassup?" + " " + str(bro)
+# b= f"hello bro waasup {bro}"
+# print(b)
+
+# scape = "hello bro \"brough\" man did what not "
+# print(scape)
+# name ="rajan"
+
+# #name.capitalize() #
+# name.
+
+# thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+# thislist.remove("banana")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# for x in range():
+#   print(x)
 
 # thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-# thislist.sort(reverse = True)
+# thislist.insert(2,"hh")
+
+# # thislist.sort(reverse = True)
+
 # print(thislist)
 
-l1 = [1,2,3,4,5]
-l2 = [6,7,8,9,10]
+# thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+# thistuple.append("mango")
+# print(thistuple)
 
-for x in l1:
-    l2.append(x)
-print(l2)
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+
+# print(x)
+
+# dic ={
+#     "name":"raj",
+#     "work":"IT Engineer",
+#     "age":24
+
+# }
+
+# dic["name"] = "syam"
+
+# print(dic["name"])
+# print(dic.popitem())
+
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for x in thisdict:
+#   print(thisdict[x])
+
+
+# myfamily = {
+#   "child1" : {
+#     "name" : "Emil",
+#     "year" : 2004
+#   },
+#   "child2" : {
+#     "name" : "Tobias",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Linus",
+#     "year" : 2011
+#   }
+# }
+
+# for x, obj in myfamily.items():
+#     print(x)
+#     # print(obj,"hello")
+    
+#     for y in obj:
+#         print(y + ':', obj[y])
+
+
+
+# print(myfamily["child1"]["name"])
+
+class Person:
+ def __init__(self,name,age):
+    self.name = name
+    self.age = age
+ def greet(self):
+    print(f"Hello my name is {self.name} and age is {self.age}")
+
+
+p1 = Person("John" , 36)
+p1.greet()  
